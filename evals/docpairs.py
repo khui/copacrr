6d4c4d3d1f302f844ea2a3a0bb6f18d8a1706f23
Abhinav Madahar <abhinavmadahar@gmail.com>
2017-12-27 13:46:45 -0500

    Ask for directories instead of assumming

--- evals/docpairs.py
+++ evals/docpairs.py
@@ -28,23 +28,23 @@ def create_docpairs(qid_cwid_label, test_qids, qid_year):
         assert qid in test_qids
         year = qid_year[qid]
         docpairs.setdefault(year, {})
-        
+
         label_cwids = {}
         for cwid, raw_label in qid_cwid_label[qid].items():
             jud = year_label_jud[year][raw_label]
             label = jud_label[jud]
             label_cwids.setdefault(label, []).append(cwid)
-           
+
         for l1, l2 in itertools.combinations(sorted(label_cwids), 2):
             pairkey = "%s-%s" % (label_jud[l2], label_jud[l1])
             docpairs[year].setdefault(pairkey, [])
 
             for dl, dh in itertools.product(label_cwids[l1], label_cwids[l2]):
                 docpairs[year][pairkey].append((qid, dl, dh))
-                
+
     return docpairs
 
-    
+
 def eval_docpair_predaccuracy(qid_cwid_score, docpairs, test_year):
     pkey_qidcount = dict()
     for pkey, docpairs in docpairs[test_year].items():
@@ -66,7 +66,7 @@ def eval_docpair_predaccuracy(qid_cwid_score, docpairs, test_year):
 
         pkey_qid_acc[pkey][0] = np.mean(list(pkey_qid_acc[pkey].values()))
         pkey_qid_acc[pkey][-1] = sum(len(outcomes) for outcomes in qids.values())
-        
+
     return pkey_qid_acc
 
 
@@ -77,7 +77,7 @@ def main(_log, _config):
     mod_model = importlib.import_module('models.%s' % p['modelfn'])
     model_cls = getattr(mod_model, modelname)
     model_params_raw = {k: v for k, v in p.items() if k in model_cls.params or k == 'modelfn'}
-    
+
     list_of_model_params = get_model_param(model_params_raw)
     expids = list()
     for model_params in list_of_model_params:
@@ -85,21 +85,23 @@ def main(_log, _config):
         expid = model.params_to_string(model_params, True)
         expids.append(expid)
     raw_expid = model.params_to_string(model_params_raw, True)
-    
+
     for train_years in train_test_years:
 
         for i in range(len(train_test_years[train_years])):
             test_year, val_year = train_test_years[train_years][i], train_test_years[train_years][1 - i]
-            
+
             pred_dirs, val_dirs = list(), list()
-            
+
             for expid in expids:
-                pred_dir = '%s/train_%s/%s/predict_per_epoch/test_%s/%s' % (p['parentdir'], train_years, p['expname'], test_year, expid)
-                val_dir = '%s/train_%s/%s/predict_per_epoch/test_%s/%s' % (p['parentdir'], train_years, p['expname'], val_year, expid)
+                pred_dir = raw_input('Enter pred_dir or leave blank to use default: ') or \
+                        '%s/train_%s/%s/predict_per_epoch/test_%s/%s' % (p['parentdir'], train_years, p['expname'], test_year, expid)
+                val_dir =  raw_input('Enter val_dir or leave blank to use default: ') or \
+                        '%s/train_%s/%s/predict_per_epoch/test_%s/%s' % (p['parentdir'], train_years, p['expname'], val_year , expid)
                 if not os.path.isdir(pred_dir) or not os.path.isdir(val_dir):
                     warnings.warn('No such dir {0}/{1}'.format(pred_dir, val_dir), RuntimeWarning)
                     continue
-                pred_dirs.append(pred_dir) 
+                pred_dirs.append(pred_dir)
                 val_dirs.append(val_dir)
             output_file='%s/train_%s/%s/evaluations/statdocpair/%s_v-%s_t-%s/%s'%(p['outdir'], train_years,\
                     p['expname'], '-'.join(train_years.split('_')), val_year[2:], test_year[2:], raw_expid)
@@ -116,7 +118,7 @@ def main(_log, _config):
             qrelf = get_qrelf(qrelfdir, test_year)
             qid_cwid_label = read_qrel(qrelf, test_qids, include_spam=False)
             year_pkey_docpairs = create_docpairs(qid_cwid_label, test_qids, qid_year)
-            
+
             best_pred_dir, argmax_epoch, argmax_run, argmax_ndcg, argmax_err = get_epoch_from_val(pred_dirs, val_dirs)
 
             qid_cwid_invrank, _, runid = read_run(os.path.join(best_pred_dir, argmax_run))
@@ -125,5 +127,9 @@ def main(_log, _config):
 
             dftable = df(pkey_qid_acc, index=sorted(list(qid_cwid_invrank.keys()))+[0, -1])
             _log.info('\n' + dftable.to_string())
-            dftable.to_csv(output_file + '.csv', float_format='%.3f', header=True, index=True, sep=',', mode='w')
+            try:
+                dftable.to_csv(output_file + '.csv', float_format='%.3f', header=True, index=True, sep=',', mode='w')
+            except IOError:
+                output_file = raw_input('Original output filename too long. Enter new filename: ')
+                dftable.to_csv(output_file + '.csv', float_format='%.3f', header=True, index=True, sep=',', mode='w')
             _log.info('finished {0} {1} {2} {3}'.format(expid, train_years, val_year, test_year))
